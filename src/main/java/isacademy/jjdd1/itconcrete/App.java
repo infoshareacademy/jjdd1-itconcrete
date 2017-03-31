ea8755ac928ca7f450027c27a20648c9f0ab8043
agatabereza <bereza.agata@gmail.com>
2017-03-31 16:16:55 +0200

    Startowy commit ze struktura projektu.

---
+++ src/main/java/isacademy/jjdd1/itconcrete/App.java
@@ -0,0 +1,13 @@
+package isacademy.jjdd1.itconcrete;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
