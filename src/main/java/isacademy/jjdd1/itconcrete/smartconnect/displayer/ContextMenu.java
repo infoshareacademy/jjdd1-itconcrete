5937c0170d06ddf4f4bdb324204b1bbe0f05faa5
katarzynadobrowolska <katarzyna.dobrowolska@hotmail.com>
2017-04-02 12:32:58 +0200

    Context menu added.

---
+++ src/main/java/isacademy/jjdd1/itconcrete/smartconnect/displayer/ContextMenu.java
@@ -0,0 +1,18 @@
+package isacademy.jjdd1.itconcrete.smartconnect.displayer;
+
+/**
+ * Created by katarzynadobrowolska on 02.04.2017.
+ */
+public class ContextMenu {
+
+    String welcomeText;
+    String priorityQuestion;
+    String resultText;
+
+    public ContextMenu(String welcomeText, String priorityQuestion, String resultText){
+        this.welcomeText = welcomeText;
+        this.priorityQuestion = priorityQuestion;
+        this.resultText = resultText;
+
+    }
+}
