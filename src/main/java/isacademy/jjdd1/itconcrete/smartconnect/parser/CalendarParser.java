604594d8b35b8efb15705f397bdaeef8022061c8
rekinlukasz <styklukasz@gmail.com>
2017-04-03 16:33:51 +0200

    added typical event values in ics file

--- src/main/java/isacademy/jjdd1/itconcrete/smartconnect/parser/CalendarParser.java
+++ src/main/java/isacademy/jjdd1/itconcrete/smartconnect/parser/CalendarParser.java
@@ -7,7 +7,31 @@ import isacademy.jjdd1.itconcrete.smartconnect.data.CalendarEvent;
  */
 public class CalendarParser {
 
-    int someInt = 1;
+//---    Typical Event Values:
+//    BEGIN:VEVENT
+//    DTSTART:20170408T070000Z
+//    DTEND:20170408T090000Z
+//    DTSTAMP:20170403T000048Z
+//    UID:11do5a4lnt0egk4q518gb3ukbg@google.com
+//    CREATED:20170402T235327Z
+//    DESCRIPTION:
+//    LAST-MODIFIED:20170402T235327Z
+//    LOCATION:Targ Węglowy
+//    SEQUENCE:0
+//    STATUS:CONFIRMED
+//    SUMMARY:katownia
+//    TRANSP:OPAQUE
+//    END:VEVENT
+
+    //---values used by application
+    String dtstart;      //format yyyymmddT000000Z date and time of event start
+    String dtend;        //format yyyymmddT000000Z date and time of event end
+    String location;
+    String summary;
+
+
+
+
 
     public CalendarEvent[] parseDataFromPath(String calendarPath){
         //TODO logic here
