d602a501b9ecc47eab0be1aa2daaf9623116830d
agatabereza <bereza.agata@gmail.com>
2017-04-06 16:51:12 +0200

    Metoda z URL usunieta, parser z zastosowaniem BufferedReadera

--- src/main/java/isacademy/jjdd1/itconcrete/smartconnect/schedule/ScheduleParser.java
+++ src/main/java/isacademy/jjdd1/itconcrete/smartconnect/schedule/ScheduleParser.java
@@ -7,15 +7,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by agatabereza on 06.04.2017.
@@ -28,13 +21,6 @@ public class ScheduleParser {
 
 
 
-    public ScheduleParser() {
-    }
-
-//    public String getTestCase() {
-//        return testCase;
-//    }
-
     public ArrayList<String[]> scheduleParser() throws Exception{
 
         //Added to allow using polish characters.
@@ -45,12 +31,10 @@ public class ScheduleParser {
 
         listOfScheduleRows = new ArrayList<String[]>();
 
-        String pathToSchedules = "/home/agatabereza/IdeaProjects/jjdd1-itconcrete/src/main/resources/rozklady_2015-09-08_13.43.01/131_20150718/131_20150718warianty1.csv";
+        String pathToSchedules = "src/main/resources/rozklady_2015-09-08_13.43.01/131_20150718/131_20150718warianty1.csv";
         File file = new File(pathToSchedules);
         String busLineNumber = file.getName().toString().substring(0,3);
 
-        //URL resource = getClass().getResource(pathToSchedules);
-        //List<String> listOfAllLines = null;
 
         BufferedReader br = null;
         String line = "";
@@ -89,38 +73,6 @@ public class ScheduleParser {
 
         }
 
-
-
-        /*try {
-            listOfScheduleRows = Files.readAllLines(Paths.get(resource.toURI()));
-            System.out.println("probujemy");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-
-        for (String singleLine : listOfAllLines) {
-
-            String[] separatedElementsFromSingleLine = singleLine.split(";");
-            listOfScheduleRows.add(separatedElementsFromSingleLine);
-            System.out.println("Current stop id is: " + separatedElementsFromSingleLine[1]);
-        }
-
-        URL resource = getClass().getResource("C:\\Users\\Ageee\\IdeaProjects\\jjdd1-itconcrete\\src\\main\\java\\isacademy\\jjdd1\\itconcrete\\smartconnect\\resources\\rozklady_2015-09-08_13.43.01\\136_20140802\\136_20140802warianty1.csv");
-        List<String> lines = null;
-        try {
-            lines = Files.readAllLines(Paths.get(resource.toURI()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        int currentLineNumber = 0;
-        for (String line : lines) {
-            currentLineNumber+=1;
-            String[] elements = line.split(",");
-        }*/
         return listOfScheduleRows;
     }
 
