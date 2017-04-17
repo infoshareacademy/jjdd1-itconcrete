cb2cc834b984b3a59e42b37287a28c8c39e09e49
katarzynadobrowolska <katarzyna.dobrowolska@hotmail.com>
2017-04-17 18:19:10 +0200

    Refactor continuation

--- src/main/java/isacademy/jjdd1/itconcrete/smartconnect/App.java
+++ src/main/java/isacademy/jjdd1/itconcrete/smartconnect/App.java
@@ -1,13 +1,9 @@
 package isacademy.jjdd1.itconcrete.smartconnect;
 
-import isacademy.jjdd1.itconcrete.smartconnect.analyzer.*;
-import isacademy.jjdd1.itconcrete.smartconnect.calendar.Journey;
-import isacademy.jjdd1.itconcrete.smartconnect.calendar.JourneyCreator;
-import isacademy.jjdd1.itconcrete.smartconnect.displayer.DisplayConnection;
+import isacademy.jjdd1.itconcrete.smartconnect.displayer.CompleteResultDisplayer;
 import isacademy.jjdd1.itconcrete.smartconnect.displayer.QuestionAsker;
 import isacademy.jjdd1.itconcrete.smartconnect.schedule.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class App {
 
@@ -17,46 +13,13 @@ public class App {
         sp.loadData();
         ArrayList<BusLine> allBusLines = sp.getArrayOfBusLines();
 
-        MinutesToBusStops minutesToBusStops = new MinutesToBusStops();
-        ConnectionSeeker connectionSeeker = new ConnectionSeeker();
-        DisplayConnection displayConnection = new DisplayConnection();
-
-        BusLineSeeker busLineSeeker = new BusLineSeeker();
-
         String homeBusStop = QuestionAsker.askForHome(allBusLines);
         String timeOfLeavingHome = QuestionAsker.askForTimeOfLeavingHome();
         String timeOfArrivingHome = QuestionAsker.askForTimeOfArrivingHome();
 
+        CompleteResultDisplayer completeResultDisplayer = new CompleteResultDisplayer();
+        completeResultDisplayer.displayCompleteResult(homeBusStop, timeOfLeavingHome, timeOfArrivingHome, allBusLines);
 
-        JourneyCreator journeyCreator = new JourneyCreator();
-        List<Journey> journeys = journeyCreator.getJourneysList(homeBusStop, timeOfLeavingHome, timeOfArrivingHome);
-
-        for (int i = 0; i < journeys.size(); i++) {
-
-            System.out.println("");
-
-            System.out.println("Event number " + (i+1) + ": ");
-
-            String textForEachEvent = displayConnection.displayEventHeader(journeys.get(i));
-            System.out.println(textForEachEvent);
-
-            List<BusLine> foundBusLines = busLineSeeker.seekBusLine(journeys.get(i), allBusLines);
-            List<LineRideTime> lineRideTimes = minutesToBusStops.calculateMinutesToBusStops(foundBusLines, journeys.get(i));
-            List<ResultConnection> resultConnections = connectionSeeker.seekConnection(lineRideTimes, journeys.get(i));
-
-            for (ResultConnection resultConnection : resultConnections) {
-                String textForEachResult = displayConnection.displayingConnection(resultConnection);
-
-                    System.out.println(textForEachResult);
-            }
-
-            if (resultConnections.size() == 0) {
-                System.out.println("Sorry, there is no direct connection for this event.");
-            }
-
-            System.out.println("");
-
-        }
     }
 }
 
