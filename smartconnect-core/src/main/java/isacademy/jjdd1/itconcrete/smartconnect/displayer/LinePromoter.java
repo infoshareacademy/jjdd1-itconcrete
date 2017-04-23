dd0aa18072177e0c9bd85fc4fde8b5d9d8a002b0
agatabereza <bereza.agata@gmail.com>
2017-04-23 20:26:00 +0200

    JD1IC-16 Functionality of line promotion has been added - class LinePromoter

---
+++ smartconnect-core/src/main/java/isacademy/jjdd1/itconcrete/smartconnect/displayer/LinePromoter.java
@@ -0,0 +1,37 @@
+package isacademy.jjdd1.itconcrete.smartconnect.displayer;
+
+import isacademy.jjdd1.itconcrete.smartconnect.analyzer.ResultConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LinePromoter {
+
+    private ArrayList<Integer> promotedLines;
+    private List<ResultConnection> resultConnections;
+
+    public LinePromoter(List<ResultConnection> resultConnections) {
+        promotedLines = new ArrayList<>();
+        this.resultConnections = resultConnections;
+    }
+
+    public List<ResultConnection> putPromotedLinesFirstInAList(){
+        promotedLines.add(157);
+        promotedLines.add(116);
+
+        List<ResultConnection> convertedList = new ArrayList<ResultConnection>();
+
+        for (int i = 0; i < resultConnections.size(); i++){
+            int currentLineNumber = resultConnections.get(i).getLineNumber();
+            ResultConnection currentConnection = resultConnections.get(i);
+            if (promotedLines.contains(currentLineNumber) && !convertedList.contains(currentConnection)){
+                convertedList.add(0, currentConnection);
+            } else {
+                convertedList.add(currentConnection);
+            }
+        }
+        return convertedList;
+    }
+    //TODO Function to get the list of promoted lines, now the type of source is unknown and values are given manually
+}
+
