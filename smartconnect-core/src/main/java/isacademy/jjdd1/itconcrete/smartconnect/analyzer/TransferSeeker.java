3774d94e0ce3e79548cb70fb52f2cb162653af17
agatabereza <bereza.agata@gmail.com>
2017-04-26 15:52:12 +0200

    JD1IC-12 Initialization of branch for unit tests

--- smartconnect-core/src/main/java/isacademy/jjdd1/itconcrete/smartconnect/analyzer/TransferSeeker.java
+++ smartconnect-core/src/main/java/isacademy/jjdd1/itconcrete/smartconnect/analyzer/TransferSeeker.java
@@ -5,6 +5,7 @@ import isacademy.jjdd1.itconcrete.smartconnect.schedule.BusStopDeltas;
 import isacademy.jjdd1.itconcrete.smartconnect.schedule.ScheduleParser;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,41 +29,41 @@ public class TransferSeeker {
 
 
     public List<List<BusLine>> seekTransfer (String startBusStop, String endBusStop)
-            throws IOException, NoSuchFieldException, IllegalAccessException {
+            throws IOException, NoSuchFieldException, IllegalAccessException, URISyntaxException {
 
-//        ScheduleParser sp = new ScheduleParser();
-//        sp.loadData();
-//        ArrayList<BusLine> allBusLines = sp.getArrayOfBusLines();
+        ScheduleParser sp = new ScheduleParser();
+        sp.loadData();
+        ArrayList<BusLine> allBusLines = sp.getArrayOfBusLines();
 
         List<BusLine> foundPartStartBusLine = new ArrayList<>();
         List<BusLine> foundPartEndBusLine = new ArrayList<>();
 
-//        List<BusLine> confirmedStartPartBusLines = seekPartBusLine(allBusLines, startBusStop);
-//        List<BusLine> confirmedEndPartBusLines = seekPartBusLine(allBusLines, endBusStop);
+        List<BusLine> confirmedStartPartBusLines = seekPartBusLine(allBusLines, startBusStop);
+        List<BusLine> confirmedEndPartBusLines = seekPartBusLine(allBusLines, endBusStop);
 
-//        for (BusLine foundStartPartBusLine : confirmedStartPartBusLines) {
-//
-//            List<BusStopDeltas> startDeltasList = foundStartPartBusLine.getRoute().getDeltasList();
-//
-//            for (BusLine foundEndPartBusLine : confirmedEndPartBusLines) {
-//
-//                List<BusStopDeltas> endDeltasList = foundEndPartBusLine.getRoute().getDeltasList();
-//
-//                for (BusStopDeltas currentStartDeltasList : startDeltasList) {
-//
-//                    for (BusStopDeltas currentEndDeltasList : endDeltasList) {
-//
-//                        if (currentStartDeltasList.equals(currentEndDeltasList)){
-//
-//                            foundPartStartBusLine.add(foundStartPartBusLine);
-//                            foundPartEndBusLine.add(foundEndPartBusLine);
-//
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
+        for (BusLine foundStartPartBusLine : confirmedStartPartBusLines) {
+
+            List<BusStopDeltas> startDeltasList = foundStartPartBusLine.getRoute().getDeltasList();
+
+            for (BusLine foundEndPartBusLine : confirmedEndPartBusLines) {
+
+                List<BusStopDeltas> endDeltasList = foundEndPartBusLine.getRoute().getDeltasList();
+
+                for (BusStopDeltas currentStartDeltasList : startDeltasList) {
+
+                    for (BusStopDeltas currentEndDeltasList : endDeltasList) {
+
+                        if (currentStartDeltasList.equals(currentEndDeltasList)){
+
+                            foundPartStartBusLine.add(foundStartPartBusLine);
+                            foundPartEndBusLine.add(foundEndPartBusLine);
+
+                        }
+                    }
+                }
+            }
+        }
+
         List<List<BusLine>> foundStartEndPartBusLine = new ArrayList<>();
 
         foundStartEndPartBusLine.add(foundPartStartBusLine);
