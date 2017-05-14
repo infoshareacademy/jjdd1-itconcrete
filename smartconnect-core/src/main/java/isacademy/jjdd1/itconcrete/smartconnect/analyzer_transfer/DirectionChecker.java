6397c07e6c7a1e7f6bf35fa532894150cd2bf8ec
katarzynadobrowolska <katarzyna.dobrowolska@hotmail.com>
2017-05-14 10:40:02 +0200

    'Going back in time' feature deleted

--- smartconnect-core/src/main/java/isacademy/jjdd1/itconcrete/smartconnect/analyzer_transfer/DirectionChecker.java
+++ smartconnect-core/src/main/java/isacademy/jjdd1/itconcrete/smartconnect/analyzer_transfer/DirectionChecker.java
@@ -1,7 +1,6 @@
 package isacademy.jjdd1.itconcrete.smartconnect.analyzer_transfer;
 
 import isacademy.jjdd1.itconcrete.smartconnect.schedule.BusStopDeltas;
-
 import java.util.List;
 
 class DirectionChecker {
@@ -15,10 +14,20 @@ class DirectionChecker {
         int startBusStopIndexFirstLine = busStopIndexCounter.countBusStopIndex(firstLineDeltasList, startBusStop);
         int midBusStopIndexFirstLine = busStopIndexCounter.countBusStopIndex(firstLineDeltasList, midBusStop);
 
+        if (startBusStopIndexFirstLine == -1 || midBusStopIndexFirstLine == -1) {
+            checkedDirection = false;
+            return checkedDirection;
+        }
+
         boolean startStopBeforeMidStopFirstLine = startBusStopIndexFirstLine < midBusStopIndexFirstLine;
 
         int midBusStopIndexSecondLine = busStopIndexCounter.countBusStopIndex(secondLineDeltasList, midBusStop);
         int endBusStopIndexSecondLine = busStopIndexCounter.countBusStopIndex(secondLineDeltasList, endBusStop);
+
+        if (midBusStopIndexSecondLine == -1 || endBusStopIndexSecondLine == -1) {
+            checkedDirection = false;
+            return checkedDirection;
+        }
 
         boolean midStopBeforeEndStopSecondLine = midBusStopIndexSecondLine < endBusStopIndexSecondLine;
 
@@ -27,10 +36,6 @@ class DirectionChecker {
         return checkedDirection;
 
     }
-
-
-
-
 
 
 }
